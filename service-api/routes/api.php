de760dd6da82da8e9c2f0dc2b49174055d60f68b
Laise <laisevn@gmail.com>
2024-02-27 19:23:39 -0300

    feat: add tasks migration, model and controller

--- service-api/routes/api.php
+++ service-api/routes/api.php
@@ -3,19 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\TaskController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::post('/user', [UserController::class, 'singup']);
+
+Route::get('/tasks', [TasksController::class, 'index']);
+Route::get('/tasks/{id}', [TasksController::class, 'show']);
+Route::post('/tasks', [TasksController::class, 'store']);
+Route::put('/tasks/{id}', [TasksController::class, 'update']);
+Route::delete('/tasks/{id}', [TasksController::class, 'destroy']);
