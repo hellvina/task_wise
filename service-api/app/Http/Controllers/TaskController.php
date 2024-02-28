b98b3b85d31e044bd7382e37945f8c2b72b76726
Laise <laisevn@gmail.com>
2024-02-27 21:37:21 -0300

    feat: task controller

--- service-api/app/Http/Controllers/TaskController.php
+++ service-api/app/Http/Controllers/TaskController.php
@@ -7,31 +7,34 @@ use Illuminate\Http\Request;
 class TaskController extends Controller
 {
 
+    public function create(Request $request) 
+    {
+        $item = Item::create($request->all());
+        return response()->json($item, 201);
+    }
+
     public function index() 
     {
-    }
-
-    public function create() 
-    {
-    }
-
-    public function store(Request $request) 
-    {
+        $items = Item::all();
+        return response()->json($itens, 200);
     }
 
     public function show(string $id) 
     {
-    }
-
-    public function edit(string $id) 
-    {
+        $item = Item::find($id);
+        return response()->json($item, 200);
     }
 
     public function update(Request $request, string $id) 
     {
+        $item = Item::find($id);
+        $item->update($request->all());
+        return response()->json($item, 200);
     }
 
     public function destroy(string $id) 
     {
+        Item::destroy($id);
+        return response()->json(null, 204);
     }
 }
