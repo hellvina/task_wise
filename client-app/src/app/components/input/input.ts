8218aa1d68a5ee0f67cfeb931e11464fe5cbb27f
Laise <laisevn@gmail.com>
2024-02-28 19:58:13 -0300

    feat: register form module

--- client-app/src/app/components/input/input.ts
+++ client-app/src/app/components/input/input.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
--- client-app/src/app/pages/register/register.ts
+++ client-app/src/app/pages/register/register.ts
@@ -1,15 +1,40 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from "../../components/button/button";
-import { InputComponent } from "../../components/input/input";
 import { TitleComponent } from "../../components/title/title";
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Router} from '@angular/router';
 
 @Component({
     selector: 'app-register',
     standalone: true,
     templateUrl: './register.html',
     styleUrl: './register.css',
-    imports: [ButtonComponent, InputComponent, TitleComponent]
+    imports: [ButtonComponent, TitleComponent, FormsModule, HttpClientModule]
 })
 export class Register {
+    name: string = "";
+    email: string = "";
+    password: string = "";
 
+    constructor(private http: HttpClient, private router: Router) {}
+
+    onSubmit(): void {
+        const formData = {
+            name: this.name,
+            email: this.email,
+            password: this.password
+        };
+
+        this.http.post<any>('http://localhost:8000/api/user/signup', formData).subscribe(
+            response => {
+                localStorage.setItem('token', response.token)
+                console.log('Response:', response);
+                this.router.navigate([''])
+            },
+            error => {
+                console.error('Error:', error)
+            }
+        )
+    }
 }
