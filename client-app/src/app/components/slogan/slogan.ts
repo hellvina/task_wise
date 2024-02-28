03d57f97596c3c188a5adbf3482ef058f5a2a7b9
Laise <laisevn@gmail.com>
2024-02-28 13:53:27 -0300

    style: change secondary font

---
+++ client-app/src/app/components/slogan/slogan.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slogan',
+  standalone: true,
+  imports: [],
+  templateUrl: './slogan.html',
+  styleUrl: './slogan.css'
+})
+export class SloganComponent {
+
+}
--- client-app/src/app/pages/index/index.ts
+++ client-app/src/app/pages/index/index.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../components/button/button';
 import { TitleComponent } from '../../components/title/title';
+import { SloganComponent } from '../../components/slogan/slogan';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [ButtonComponent, TitleComponent],
+  imports: [ButtonComponent, TitleComponent, SloganComponent],
   templateUrl: './index.html',
   styleUrl: './index.css'
 })
